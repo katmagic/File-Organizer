a6bf0ca9f607a12879aeaa94f488bcaaf4be238b
katmagic <the.magical.kat@gmail.com>
2011-01-02 04:06:45 -0500

    Make FileOrganizer.organize() a command that can be activated from the Tools menu instead of something that's run automatically.

--- file_organizer.py
+++ file_organizer.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 import rb
 import rhythmdb
+import gtk
 
 class _RDBEntry(object):
 	"""Our attributes are RhythmDB information."""
@@ -23,17 +24,36 @@ class FileOrganizer(rb.Plugin):
 		self.new_path = "{0.artist}/{0.album}/{0.track_number} - {0.title}.mp3"
 		self.shell = shell
 		self.rdb = shell.get_property("db")
+		self.uim = shell.get_ui_manager()
 
-		self.organize()
+		self.add_organize_tool()
+
+	def add_organize_tool(self):
+		"""Add an 'Organize Files' entry to the 'Tools' menu associated with
+		self.organize()."""
+
+		action = gtk.Action(
+			"OrganizeFiles",
+			"Organize Files",
+			"Rename files on the basis of their metadata.",
+			"OrganizeFiles"
+		)
+		action.connect('activate', self.organize, self.shell)
+
+		action_group = gtk.ActionGroup('OrganizeFilesActionGroup')
+		action_group.add_action(action)
+		self.uim.insert_action_group(action_group)
+
+		self.uim.add_ui_from_file( self.find_file("file_organizer.xml") )
 
 	def deactivate(self, shell):
 		"""This is called when we're deactivated or RhythmBox exits."""
 
 		print("deactivating")
 
-		del self.shell, self.rdb
+		del self.shell, self.rdb, self.uim
 
-	def organize(self):
+	def organize(self, *_): # The arguments that go into _ are useless.
 		"""Rename all the music in our database based on its metadata."""
 
 		self.rdb.entry_foreach(lambda e: self.organize_single_entry(e))
